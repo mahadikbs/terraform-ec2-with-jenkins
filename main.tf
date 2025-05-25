7bc7d1175c1c637d5694ae4908bebe5253d984b1
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-05-25 09:32:48 +0530

    update changes

--- main.tf
+++ main.tf
@@ -11,33 +11,19 @@ resource "aws_instance" "terraform-test" {
     vpc_security_group_ids = var.security-group
 
     tags = {
-      Name = "tf-Jenkins-and-prometheus-1012434"
+      Name = "tf-Jenkins-1012434"
     }
 
     provisioner "file" {
         source = "jenkins.sh"
         destination = "/home/ec2-user/install-jenkins.sh"
   } 
-    provisioner "file" {
-        source = "prom/prom1.sh"
-        destination = "/home/ec2-user/install-prom1.sh"
-}      
-    provisioner "file" {
-        source = "prom/prom2.sh"
-        destination = "/home/ec2-user/install-prom2.sh"
-} 
-    provisioner "file" {
-        source = "prom/prom.service"
-        destination = "/home/ec2-user/prometheus.service"
-}     
+
     provisioner "remote-exec" {
         inline = [ 
             "sudo cd /home/ec2-user/",
             "sudo chmod +x install-jenkins.sh",
-            "sudo ./install-jenkins.sh",
-            "echo installing prometheus",
-            "sudo chmod +x install-prom1.sh",
-            "sudo ./install-prom1.sh"      
+            "sudo ./install-jenkins.sh"    
          ]
     }
 
@@ -47,35 +33,4 @@ resource "aws_instance" "terraform-test" {
       private_key = file("~/.ssh/id_rsa")
       host = self.public_ip
     }
-}
-
-# generate prometheus config file with public ip
-
-resource "template_file" "prometheus-config" {
-    template = file("prom/prometheus_conf.yml.tpl")
-
-    vars = {
-      PUBLIC_IP = aws_instance.terraform-test.public_ip
-    }
-
-provisioner "remote-exec" {
-    inline = [ 
-        "echo '${template_file.prometheus_conf.rendered} | sudo tee /etc/prometheus/prometheus.yml",
-        "sudo -u prometheus /usr/local/bin/prometheus --config.file /etc/prometheus/prometheus.yml --storage.tsdb.path /var/lib/prometheus --web.console.templates=/etc/prometheus/consoles --web.console.libraries=/etc/prometheus/console_libraries",
-        "sudo cp -rv /home/ec2-user/prometheus.service /etc/systemd/system/",
-        "sudo cd /home/ec2-user/",
-        "sudo chmod +x install-prom2.sh",
-        "sudo ./install-prom2.sh"        
-     ]
-
-}
-
-    connection {
-      type = "ssh"
-      user = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      host = aws_instance.terraform-test.public_ip
-    }
-
-
 }
